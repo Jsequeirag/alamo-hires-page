ca4b27d407ac959a2be1d13b01a156a68ff0b777
Jsequeirag <99283803+Jsequeirag@users.noreply.github.com>
2025-07-24 22:59:35 -0600

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,14 @@
-import AlamoJobs from './AlamoJobs';
 
-const Index = () => {
-  return <AlamoJobs />;
+import React from 'react';
+import AppLayout from '@/components/AppLayout';
+import { AppProvider } from '@/contexts/AppContext';
+
+const Index: React.FC = () => {
+  return (
+    <AppProvider>
+      <AppLayout />
+    </AppProvider>
+  );
 };
 
 export default Index;
